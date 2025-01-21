f167cd39e3300ad714d55b4a46fbaedb5c186f0c
FikretZajmovic <fikret.zajmovic@stu.ibu.edu.ba>
2025-01-21 17:18:37 +0100

    Defining data model

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,11 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
